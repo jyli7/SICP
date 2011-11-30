cd96bc3fbe8fbd3625acf6c808b05039beb9aa81
jimmyli <jyl702@gmail.com>
2011-11-30 14:54:44 -0500

    Deleted Chapter 1 files

--- Chapter 1/ex1.14.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex1.14) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-;Ex 1.14
-
-;Space and steps are both either linear or logarithmic?
-
-;11 --> 1 with 10
-;11 w/o 10
